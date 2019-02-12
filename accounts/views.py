51e29a0a15d4e2e1942f23c049495947ff4bdad1
Maciej Loj <maciej.loj@gmail.com>
2019-02-12 02:31:12 +0100

    Update

--- accounts/views.py
+++ accounts/views.py
@@ -9,8 +9,14 @@ def login(request):
 
 
 def register(request):
-    form = UserCreationForm()
-    return render(request,'accounts/register.html',{'form':form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('recipes:list')
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/register.html',{'form': form})
 
 
     # if request.method == 'POST':
