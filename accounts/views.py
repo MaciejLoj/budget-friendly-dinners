ebbf1a1d4b6d598a19146ac7d778f45d8ecbe74c
Maciej Loj <maciej.loj@gmail.com>
2019-02-15 00:18:00 +0100

    Update

--- accounts/views.py
+++ accounts/views.py
@@ -5,13 +5,18 @@ from django.contrib.auth import login, logout
 
 # Create your views here.
 
+
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.get_user()
             login(request,user) # logs you in after signing up
             # messages.success(request, 'Account created successfully')
+            # next to name z login.html
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            # przenosi nas .get do naszego next, ktory wzielismy z urla  login.html
             return redirect('recipes:list')
     else:
         form = AuthenticationForm()
@@ -22,12 +27,13 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.get_user()
+            user = form.save()
             login(request,user)
             return redirect('recipes:list')
     else:
         form = UserCreationForm()
     return render(request,'accounts/register.html',{'form': form})
+
 
 def logout_view(request):
     if request.method == 'POST':
--- dinners/urls.py
+++ dinners/urls.py
@@ -7,6 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'dinners'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
