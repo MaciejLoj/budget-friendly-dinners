acf463400ec9963b0b2bbbe71d6e084710f2f45f
Maciej Loj <maciej.loj@gmail.com>
2019-02-16 00:28:31 +0100

    Update

---
+++ static/js/slug_creator.js
@@ -0,0 +1,15 @@
+const titleInput = document.querySelector('input[name=title]');
+const slugInput = document.querySelector('input[name=slug]');
+
+const slug_creator = (val) => {
+    return val.toString().toLowerCase().trim()
+        .replace(/&/g,'-and-')
+        .replace(/[\s\W-]+/g,'-')
+
+};
+
+
+titleInput.addEventListener('keyup',(e)=>{
+    slugInput.setAttribute('value',slug_creator(titleInput.value))
+
+});
