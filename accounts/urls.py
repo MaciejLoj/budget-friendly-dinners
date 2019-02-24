c5a005a295da1af4cc4d41cbe41792bec4785ca6
Maciej Loj <maciej.loj@gmail.com>
2019-02-25 00:30:52 +0100

    Password reset update

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('logowanie/',views.login_view, name='login'),
     path('rejestracja/', views.register, name='register'),
     path('wylogowanie/', views.logout_view, name='logout'),
-    path('password_reset/',auth_views.PasswordResetView,name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.PasswordResetConfirmView, name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView, name='password_reset_complete')
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
