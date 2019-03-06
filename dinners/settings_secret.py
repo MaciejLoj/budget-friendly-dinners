bd84f9a0cafe8705a7e2bba39883bc4fd094daf0
Maciej Loj <maciej.loj@gmail.com>
2019-03-06 01:55:20 +0100

    Hid more credentials

--- dinners/settings_secret.py
+++ dinners/settings_secret.py
@@ -1,3 +1,3 @@
 SECRET_KEY = 'fx68mn@z8qa30@(rqeirvdopjdisep#fjm+3l*)s0%4vy-l%+q'
 EMAIL_HOST_USER = 'mlojpanattoni@gmail.com'
-EMAIL_HOST_PASSWORD = 'Panattoni2018'
+EMAIL_HOST_PASSWORD = 'Plecto2019'
