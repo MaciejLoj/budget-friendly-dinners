fa92c1f3bb755ab43b1fd5cd47c22176d93c7356
Maciej Loj <maciej.loj@gmail.com>
2019-04-27 00:26:48 +0200

    Uodated recipes' views

--- recipes/views.py
+++ recipes/views.py
@@ -3,24 +3,32 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic import DetailView, ListView
+from django.shortcuts import render, redirect
+from .forms import CreateRecipe
 
 
-class FiveZlotysDinners(View):
+class FiveZlotysRecipes(ListView):
 
-    def get(self, request):
-        pass
-
-
-class TenZlotysDinners(View):
-
-    def get(self, request):
-        pass
+    model = Recipe
+    context_object_name = 'sorted_recipes'
+    queryset = Recipe.objects.filter(price='do 5 PLN')
+    template_name = 'recipes/recipes_base_template.html'
 
 
-class TwentyZlotysDinners(View):
+class TenZlotysRecipes(ListView):
 
-    def get(self, request):
-        pass
+    model = Recipe
+    context_object_name = 'sorted_recipes'
+    queryset = Recipe.objects.filter(price='do 10 PLN')
+    template_name = 'recipes/recipes_base_template.html'
+
+
+class FifteenZlotysRecipes(ListView):
+
+    model = Recipe
+    context_object_name = 'sorted_recipes'
+    queryset = Recipe.objects.filter(price='do 15 PLN')
+    template_name = 'recipes/recipes_base_template.html'
 
 
 class RecipeDetail(DetailView):
@@ -38,9 +46,22 @@ class NewRecipes(ListView):
     model = Recipe
     context_object_name = 'recipes'
     queryset = Recipe.objects.all().order_by('date')
-    template_name = 'recipes/new_recipes.html'
+    template_name = 'recipes/all_recipes.html'
 
 
 @method_decorator(login_required, name='dispatch')
-class ProtectedView(View):
+class CreateRecipeView(View):
+    form_class = CreateRecipe
     template_name = 'recipes/create_recipe.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            recipe = form.save()
+            recipe.save()
+            return redirect('/przepisy')
+        return render(request, self.template_name, {'form': form})
